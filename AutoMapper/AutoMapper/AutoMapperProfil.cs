3b15b8e0210a05fcc603783c632294b7b4c44add
AnasChelli <37775415+AnasChelli@users.noreply.github.com>
2021-03-06 22:03:31 +0100

    First Commit

---
+++ AutoMapper/AutoMapper/AutoMapperProfil.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoMapperExtension
+{
+    public class AutoMapperProfil : Profile
+    {
+     //   CreateMap<Client, ClientDto>();
+    }
+}
---
+++ AutoMapper/AutoMapper/Program.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapperExtension;
+using System;
+
+namespace AutoMapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<Client, ClientDto>().ReverseMap());
+
+            var clientDto = new ClientDto
+            {
+                FirstName = "Anas",
+                LastName = "CHELLY",
+                Address = "France",
+                Email = "anaschelly@gmail.com",
+                Phone = "010000000"
+            };
+
+            var mapper = new Mapper(config);
+            var client = mapper.Map<Client>(clientDto);
+
+            Console.WriteLine($"Client information : {client.FirstName} {client.LastName} {client.Address} {client.Email} {client.Phone}");
+        }
+    }
+}
